851134f8a475ff4fe5cf93f6e7bcbf7cc672cdf2
wrawka <wrawka@gmail.com>
2022-06-19 12:48:40 +0300

    Link click counter implemented

--- shortener_app/crud.py
+++ shortener_app/crud.py
@@ -14,6 +14,7 @@ def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
 
 
 def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
+    """Create new short URL database record."""
     key = keygen.create_unique_random_key(db=db)
     secret_key = f"{key}_{keygen.create_random_key(length=8)}"
     db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
@@ -30,3 +31,11 @@ def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
         .filter(models.URL.secret_key == secret_key, models.URL.is_active)
         .first()
     )
+
+
+def update_db_clicks(db: Session, db_url: schemas.URL) -> models.URL:
+    """Increment short URL link click counter."""
+    db_url.clicks += 1
+    db.commit()
+    db.refresh(db_url)
+    return db_url
--- shortener_app/main.py
+++ shortener_app/main.py
@@ -48,6 +48,7 @@ def read_root():
 @app.get("/{url_key}")
 def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
     if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        crud.update_db_clicks(db=db, db_url=db_url)
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
