1777820097bcb909494b2dc54255a3e80fa6e3d5
wrawka <wrawka@gmail.com>
2022-06-19 12:33:21 +0300

    app MVP

---
+++ shortener_app/crud.py
@@ -0,0 +1,32 @@
+# shortener_app/crud.py
+
+from sqlalchemy.orm import Session
+
+from . import keygen, models, schemas
+
+
+def get_db_url_by_key(db: Session, url_key: str) -> models.URL:
+    return (
+        db.query(models.URL)
+        .filter(models.URL.key == url_key, models.URL.is_active)
+        .first()
+    )
+
+
+def create_db_url(db: Session, url: schemas.URLBase) -> models.URL:
+    key = keygen.create_unique_random_key(db=db)
+    secret_key = f"{key}_{keygen.create_random_key(length=8)}"
+    db_url = models.URL(target_url=url.target_url, key=key, secret_key=secret_key)
+    db.add(db_url)
+    db.commit()
+    db.refresh(db_url)
+    return db_url
+
+
+def get_db_url_by_secret_key(db: Session, secret_key: str) -> models.URL:
+    """Checks database for an active database entry with the provided `secret_key`."""
+    return (
+        db.query(models.URL)
+        .filter(models.URL.secret_key == secret_key, models.URL.is_active)
+        .first()
+    )
---
+++ shortener_app/main.py
@@ -0,0 +1,69 @@
+# shortener_app/main.py
+
+import validators
+from fastapi import Depends, FastAPI, HTTPException, Request
+from fastapi.responses import RedirectResponse
+from sqlalchemy.orm import Session
+from starlette.datastructures import URL
+
+from . import crud, models, schemas
+from .config import get_settings
+from .database import SessionLocal, engine
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    """Completes "short" and "admin" URLs with base URL from settings."""
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for("administration info", secret_key=db_url.secret_key)
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
+
+
+def raise_bad_request(message):
+    raise HTTPException(status_code=400, detail=message)
+
+
+def raise_not_found(request):
+    message = f"URL '{request.url}' doesn't exist"
+    raise HTTPException(status_code=404, detail=message)
+
+
+@app.get("/")
+def read_root():
+    return "Welcome to the URL shortener API :)"
+
+
+@app.get("/{url_key}")
+def forward_to_target_url(url_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_key(db=db, url_key=url_key):
+        return RedirectResponse(db_url.target_url)
+    else:
+        raise_not_found(request)
+
+
+@app.post("/url", response_model=schemas.URLInfo)
+def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
+    if not validators.url(url.target_url):
+        raise_bad_request(message="Your provided URL is not valid")
+    db_url = crud.create_db_url(db=db, url=url)
+    return get_admin_info(db_url)
+
+
+@app.get("/admin/{secret_key}", name="administration info", response_model=schemas.URLInfo)
+def get_url_info(secret_key: str, request: Request, db: Session = Depends(get_db)):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
